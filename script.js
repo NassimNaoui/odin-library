21b4fdb7546c2acf0c75352ac69f16be438ee57f
Nassanalytics <nassim.naoui@gmail.com>
2024-04-20 15:13:03 +0200

    ajout du popup

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// construction de l'objet librarie et fonction pour ajouter des éléments
+
+
 const myLibrary = []
 
 const book = (bookName, author, read, liked) => {
@@ -9,12 +12,54 @@ const book = (bookName, author, read, liked) => {
     }
 }
 
-function addBookBtoLibrary (bookName, author, read, liked) {
+function addBookToLibrary (bookName, author, read, liked) {
     newBook = book(bookName, author, read, liked)
     return myLibrary.push(newBook)
 }
 
-addBookBtoLibrary('x','y', false, false)
-addBookBtoLibrary('Al quran', 'Allah', true, true)
-addBookBtoLibrary("harry potter à l'école des sorciers", 'JK Rowling', false, true)
-console.log(myLibrary)
+function strToBoolean(str) {
+    return str === 'Yes';
+}
+
+const submit = document.querySelector(".submit");
+const authorName = document.querySelector("#author");
+const bookName = document.querySelector("#book");
+const readInput = document.querySelector("#readInput");
+const lovedItInput = document.querySelector("#lovedInput");
+let readChoice = '';
+let lovedItChoice = '';
+
+readInput.addEventListener('input', function(){
+    readChoice = readInput.value.trim();
+})
+
+lovedItInput.addEventListener('input', function(){
+    lovedItChoice = readInput.value.trim();
+})
+
+submit.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    const author = authorName.value;
+    const book = bookName.value;
+    const read = strToBoolean(readChoice); // Convert readChoice to boolean
+    const liked = strToBoolean(lovedItChoice); // Convert lovedItChoice to boolean
+
+    addBookToLibrary(author, book, read, liked);
+
+    console.log(myLibrary);
+});
+
+// fonction pour afficher le pop up et le fermer
+
+const popup = document.querySelector(".popup")
+const showPopUp = document.querySelector(".add")
+const closePopUp = document.querySelector(".close-btn")
+
+showPopUp.addEventListener("click", function() {
+    popup.classList.add("active");
+})
+
+closePopUp.addEventListener("click", function() {
+    popup.classList.remove("active");
+})
